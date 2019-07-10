85e69c533b2d5e8dea8d654e86a4405b0ca82338
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-07-10 17:38:57 -0400

    Style rover photos

--- src/RoverDisplay.js
+++ src/RoverDisplay.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function RoverDisplay(props) {
     return (
-        <div>
-            {typeof(props.photos)==='string' && props.photos}
+        <ImageContainer>
+            {typeof(props.photos)==='string' && <h1>{props.photos}</h1>}
             {typeof(props.photos)==='object' && props.photos.map(photo => {
                 return <img src={`${photo.img_src}`} />
             })}
-        </div>
+        </ImageContainer>
     )
 }
 
 export default RoverDisplay
+
+const ImageContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+
+    h1{
+        text-align: center;
+    }
+    img{
+        border-radius: 15px;
+        width: 22%;
+        padding: 5px 0;
+    }    
+`
