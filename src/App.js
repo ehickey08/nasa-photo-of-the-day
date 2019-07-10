14c14a59562f1b1a07239e7b368987ebc76a3009
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-07-10 15:36:36 -0400

    Use antd to add datepicker to fetch new photo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios'
 import "./App.css";
 import Display from './Display'
+import {DatePicker, Spin} from 'antd'
+import moment from 'moment'
+
 function App() {
     const [photoDetails, setPhotoDetails] = useState({url: '', explanation: '', title: '', media_type: ''})
     const [chosenDate, setChosenDate] = useState('2019-07-10')
+    const [isLoading, setIsLoading] = useState(true)
 
     function getPhoto(url, date){
         axios 
-            .get(url)
+            .get(`${url}&date=${date}`)
             .then(res => {
                 setPhotoDetails({
                     url: res.data.url,
@@ -16,6 +20,7 @@ function App() {
                     title: res.data.title,
                     type: res.data.media_type
                 })
+                setIsLoading(false)
             })
             .catch(err => {
                 console.log(err)
@@ -24,12 +29,13 @@ function App() {
 
     useEffect(() => {
         getPhoto(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`, chosenDate)
-    }, [])
+    }, [chosenDate])
 
   return (
     <div className="app_container">
         <h1 className="title">{photoDetails.title}</h1>
-        <Display media={photoDetails.type} url={photoDetails.url}/>
+        <DatePicker defaultValue={moment(`${chosenDate}`, `YYYY-MM-DD`)} onChange={(date, dateString) => setChosenDate(dateString)}/>
+        {isLoading ? <Spin size='large' /> : <Display media={photoDetails.type} url={photoDetails.url}/>}
         <h4>{chosenDate}</h4>
         <p>{photoDetails.explanation}</p>
     </div>
